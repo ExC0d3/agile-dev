fc49a90aa9e3ebe54161deee7e7b48237528291b
Abhinav <sharma.abhinav503@gmail.com>
2015-06-17 12:38:45 +0530

    Cart And Line Item Controller Update

---
+++ app/controllers/concerns/current_cart.rb
@@ -0,0 +1,12 @@
+module CurrentCart
+	extend ActiveSupport::Concernt
+
+	private
+
+		def set_cart
+			@cart = Cart.find(session[:card_id])
+		rescue  ActiveRecord::RecordNotFound
+			@cart = Cart.create
+			session[:card_id] =   @cart.id
+		end
+	end
